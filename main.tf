837102f827d9e2e7c589b423482fcc4d869428ee
RohitSquareops <rohit.kumar@squareops.com>
2023-05-30 22:55:54 +0530

    Initial Release

--- main.tf
+++ main.tf
@@ -13,11 +13,10 @@ resource "random_password" "password" {
   special = false
 }
 
-
 resource "aws_elasticache_parameter_group" "default" {
   name        = "${var.environment}-${var.name}-redis-parameter-group"
-  description = var.parameter_group_description != null ? var.parameter_group_description : "Elasticache parameter group for ${var.environment}-${var.name}"
   family      = var.family
+  description = var.parameter_group_description != null ? var.parameter_group_description : "Elasticache parameter group for ${var.environment}-${var.name}"
   tags = {
     Name        = "${var.environment}-${var.name}-redis-parameter-group"
     Environment = var.environment
@@ -29,27 +28,28 @@ resource "aws_elasticache_parameter_group" "default" {
     ]
   }
 }
+
 resource "aws_elasticache_replication_group" "redis" {
   replication_group_id       = "${var.environment}-${var.name}-redis"
-  description                = "Redis cluster for ${var.environment}-${var.name}-redis"
-  engine                     = "redis"
-  engine_version             = var.engine_version
   port                       = var.port
+  engine                     = "redis"
   node_type                  = var.node_type
+  description                = "Redis cluster for ${var.environment}-${var.name}-redis"
+  engine_version             = var.engine_version
   num_cache_clusters         = var.num_cache_nodes
   parameter_group_name       = join("", aws_elasticache_parameter_group.default.*.name) #var.parameter_group_name
   security_group_ids         = [module.security_group_redis.security_group_id]
   subnet_group_name          = aws_elasticache_subnet_group.elasticache.id
   availability_zones         = [for n in range(0, var.availability_zones) : data.aws_availability_zones.available.names[n]]
-  automatic_failover_enabled = var.automatic_failover_enabled
+  snapshot_arns              = var.snapshot_arns
   snapshot_window            = var.snapshot_window
   snapshot_retention_limit   = var.snapshot_retention_limit
-  snapshot_arns              = var.snapshot_arns
+  automatic_failover_enabled = var.automatic_failover_enabled
   multi_az_enabled           = var.multi_az_enabled
   kms_key_id                 = var.kms_key_arn
+  auth_token                 = var.transit_encryption_enabled ? random_password.password.result : null
   at_rest_encryption_enabled = var.at_rest_encryption_enabled
   transit_encryption_enabled = var.transit_encryption_enabled
-  auth_token                 = var.transit_encryption_enabled ? random_password.password.result : null
   notification_topic_arn     = var.notification_topic_arn
   maintenance_window         = var.maintenance_window
   final_snapshot_identifier  = var.final_snapshot_identifier
@@ -57,23 +57,22 @@ resource "aws_elasticache_replication_group" "redis" {
   dynamic "log_delivery_configuration" {
     for_each = local.slow_log
     content {
+      log_type         = "slow-log"
+      log_format       = var.slow_log_format
       destination      = var.slow_log_destination
       destination_type = var.slow_log_destination_type
-      log_format       = var.slow_log_format
-      log_type         = "slow-log"
     }
   }
 
   dynamic "log_delivery_configuration" {
     for_each = local.engine_log
     content {
+      log_type         = "engine-log"
+      log_format       = var.engine_log_format
       destination      = var.engine_log_destination
       destination_type = var.engine_log_destination_type
-      log_format       = var.engine_log_format
-      log_type         = "engine-log"
     }
   }
-
 
   tags = {
     Name        = "${var.environment}-${var.name}"
@@ -83,8 +82,8 @@ resource "aws_elasticache_replication_group" "redis" {
 
 resource "aws_elasticache_subnet_group" "elasticache" {
   name        = "${var.environment}-${var.name}-redis"
-  description = "Elastic-cache Redis subnet-group"
   subnet_ids  = var.subnets
+  description = "Elastic-cache Redis subnet-group"
   tags = {
     Name        = "${var.environment}-${var.name}-redis"
     Environment = var.environment
@@ -97,21 +96,19 @@ resource "aws_security_group_rule" "default_ingress" {
   description = "From allowed SGs"
 
   type                     = "ingress"
-  from_port                = var.port
   to_port                  = var.port
+  from_port                = var.port
   protocol                 = "tcp"
-  source_security_group_id = element(var.allowed_security_groups, count.index)
   security_group_id        = module.security_group_redis.security_group_id
+  source_security_group_id = element(var.allowed_security_groups, count.index)
 }
 
 resource "aws_security_group_rule" "cidr_ingress" {
-  count = length(var.allowed_cidr_blocks) > 0 ? length(var.allowed_cidr_blocks) : 0
-
-  description = "From allowed CIDRs"
-
+  count             = length(var.allowed_cidr_blocks) > 0 ? length(var.allowed_cidr_blocks) : 0
+  description       = "From allowed CIDRs"
   type              = "ingress"
-  from_port         = var.port
   to_port           = var.port
+  from_port         = var.port
   protocol          = "tcp"
   cidr_blocks       = element(var.allowed_cidr_blocks, count.index)
   security_group_id = module.security_group_redis.security_group_id
@@ -120,15 +117,15 @@ resource "aws_security_group_rule" "cidr_ingress" {
 module "security_group_redis" {
   source      = "terraform-aws-modules/security-group/aws"
   version     = "4.13.0"
-  create      = true
   name        = format("%s-%s-%s", var.environment, var.name, "redis-sg")
-  description = "Elastic-cache Redis security group"
+  create      = true
   vpc_id      = var.vpc_id
+  description = "Elastic-cache Redis security group"
 
   egress_with_cidr_blocks = [
     {
-      from_port   = 0
       to_port     = 0
+      from_port   = 0
       protocol    = "-1"
       cidr_blocks = "0.0.0.0/0"
     },
